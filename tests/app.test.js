8fce6916ee3e32bb8111372adbce905a51360ee6
silverraining <www.stella.choi@gmail.com>
2025-05-09 11:22:53 +0900

    code modified

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,2 +1,10 @@
 const request = require("supertest");
 const app = require("../app"); // app.js 파일 경로에 맞게 수정하세요
+
+describe("GET /", () => {
+  it(`기본 경로(/)는 200 상태코드와 함께 "Hello World!"를 반환합니다.`, async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("Hello World!");
+  });
+});
